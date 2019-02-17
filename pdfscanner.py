36b7468d62591cc1a801aa167934ce2ae28efb9a
Telolahy <stephano.telolahy@gmail.com>
2019-02-17 19:12:42 +0100

    Refactor

--- pdfscanner.py
+++ pdfscanner.py
@@ -4,53 +4,61 @@ import re
 import time
 import PyPDF2
 
+
 def getPageCount(pdf_file):
-	pdfFileObj = open(pdf_file, 'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	pages = pdfReader.numPages
-	return pages
+    pdfFileObj = open(pdf_file, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pages = pdfReader.numPages
+    return pages
+
 
 def extractData(pdf_file, page):
-	pdfFileObj = open(pdf_file, 'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	pageObj = pdfReader.getPage(page)
-	data = pageObj.extractText()
-	return data
+    pdfFileObj = open(pdf_file, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pageObj = pdfReader.getPage(page)
+    data = pageObj.extractText()
+    return data
+
 
 def getWordCount(data):
-	data = data.split()
-	return len(data)
+    data = data.split()
+    return len(data)
+
 
 def getWords(data):
     return data.split()
 
+
+def extractWords(pdf_file):
+    allWords = []
+    numPages = getPageCount(pdf_file)
+    for i in range(numPages):
+        text = extractData(pdf_file, i)
+        words = getWords(text)
+        for j in words:
+        	allWords.append(j)
+    return allWords
+
 def main():
-	if len(sys.argv)!=2:
-		print('command usage: python word_count.py FileName')
-		exit(1)
-	else:
-		pdfFile = sys.argv[1]
-	
-		# check if the specified file exists or not
-		try:
-			if os.path.exists(pdfFile):
-				print("Scanning ", pdfFile, " ...")
-		except OSError as err:
-			print(err.reason)
-			exit(1)
+    if len(sys.argv) != 2:
+        print('command usage: python word_count.py FileName')
+        exit(1)
+    else:
+        pdfFile = sys.argv[1]
 
+        # check if the specified file exists or not
+        try:
+            if os.path.exists(pdfFile):
+                print(pdfFile, " found !")
+        except OSError as err:
+            print(err.reason)
+            exit(1)
 
-		# get the words in the pdf file
-		allWords = []
-		numPages = getPageCount(pdfFile)
-		for i in range(numPages):
-			text = extractData(pdfFile, i)
-			words = getWords(text)
-			for j in words:
-    				allWords.append(j)
-		
-		time.sleep(1)
-		print(allWords)
+        # get the words in the pdf file
+        allWords = extractWords(pdfFile)
+        time.sleep(1)
+        print(allWords)
+
 
 if __name__ == '__main__':
-	main()
+    main()
